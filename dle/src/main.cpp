0128b2c4ba8e5e9e6cb66b8583d7530996104569
DaCodeChick <peepvtuber@gmail.com>
2026-03-03 00:10:22 -0500

    feat(dle): migrate icons and toolbars from original DLE
    - Copy application icons (dle.ico, dlexp.ico) from original DLE
    - Copy toolbar bitmaps (toolbar.bmp, toolbar_large.bmp)
    - Create Qt resource file (resources.qrc) for embedding
    - Set application icon, name, and version in main.cpp
    - Add AUTORCC to CMake for automatic resource compilation
    
    Resources migrated from /home/admin/Downloads/dle-src/resource/
    Icons and toolbars can now be referenced via Qt resource system (:/icons/, :/toolbars/)

--- dle/src/main.cpp
+++ dle/src/main.cpp
@@ -1,8 +1,18 @@
 #include <QApplication>
+#include <QIcon>
 #include "ui/mainwindow/MainWindow.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
+    
+    // Set application info
+    app.setApplicationName("DLE");
+    app.setApplicationDisplayName("Descent Level Editor");
+    app.setApplicationVersion("2.0.0");
+    app.setOrganizationName("Descent Community");
+    
+    // Set application icon
+    app.setWindowIcon(QIcon(":/icons/dlexp.ico"));
     
     dle::MainWindow window;
     window.show();
